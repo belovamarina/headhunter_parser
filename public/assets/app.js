712b931a2381c5e73cc695a785f97c3d74ef8625
Marina Belova <belova.ma.ev@gmail.com>
2016-10-11 17:40:46 +0300

    Add ajax refresh and fix views

---
+++ public/assets/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+// will call refreshPartial every 5 seconds
+  setInterval(refreshPartial, 15000)
+});
+
+// calls action refreshing the partial
+function refreshPartial() {
+  $.ajax({
+    url: "/"
+ })
+}
